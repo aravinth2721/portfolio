d3dfe0672b32ab0a0b0e9015900398e4d9fcb4db
Aravinth <aravinth2721@gmail.com>
2025-04-21 17:02:02 +0530

    Page Not Found page create & route config - 21 Apr 2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
     loadComponent: () =>
       import('./components/home/home.component').then((c) => c.HomeComponent),
   },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/page-not-found/page-not-found.component').then(
+        (c) => c.PageNotFoundComponent
+      ),
+  },
 ];
