e28bfc8e23d924c83aa0498f4df41de810e1c480
Aravinth <aravinth2721@gmail.com>
2025-04-21 17:33:47 +0530

    Nav component created & updated - 21 Apr 2025

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../components/nav/nav.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
